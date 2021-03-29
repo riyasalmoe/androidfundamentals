268fa98288fcbcefa9f0c1a4d26f9a384db04c24
Riyas Aboobaker <riyas.almoe@gmail.com>
2021-03-29 16:08:30 +0400

    use lists in kotlin

--- app/src/main/java/com/almoe/androidfundamentalscourse/Lists.kt
+++ app/src/main/java/com/almoe/androidfundamentalscourse/Lists.kt
@@ -86,4 +86,102 @@ fun main() {
     for (name in names) {
         println("$name - Number of characters: ${name.length}")
     }
+
+    //list use case example
+    println("")
+    println("list use case example")
+    println("=======================")
+    listUseCase()
+
+    //order class
+    println("")
+    println("class and list use case")
+    println("=======================")
+    val ordersList = mutableListOf<Order>()
+
+    val order1 = Order(1)
+    order1.addItem(Noodles())
+//    order1.print()
+    ordersList.add(order1)
+//    println()
+
+    val order2 = Order(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vegetables())
+//    order2.print()
+    ordersList.add(order2)
+//    println()
+
+    val order3 = Order(3)
+    val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
+    order3.addAll(items)
+//    order3.print()
+    ordersList.add(order3)
+
+    val order4 = Order(4).addItem(Noodles()).addItem(Vegetables("Cabbage", "Onion", "Capsicum"))
+    ordersList.add(order4)
+
+    ordersList.add(
+        Order(5)
+            .addItem(Noodles())
+            .addItem(Noodles())
+            .addItem(Vegetables("Spinach", "Cabbage"))
+    )
+
+    for (order in ordersList) {
+        order.print()
+        println("=============================")
+    }
+}
+
+class Order(val orderNumber: Int) {
+    private val itemList = mutableListOf<Item>()
+
+    fun addItem(newItem: Item): Order {
+        itemList.add(newItem)
+        return this
+    }
+
+    fun addAll(newItems: List<Item>): Order {
+        itemList.addAll(newItems)
+        return this
+    }
+
+    fun print() {
+        println("Order #$orderNumber")
+        var total = 0
+        for (item in itemList) {
+            println("$item : $${item.price}")
+            total += item.price
+        }
+        println("Total: $${total}")
+    }
+}
+
+open class Item(val name: String, val price: Int)
+
+class Noodles : Item("Noodles", 10) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+//Only one parameter can be marked as vararg and is usually the last parameter in the list.
+class Vegetables(vararg val toppings: String) : Item("Vegetables", 5) {
+    override fun toString(): String {
+        return if (toppings.isEmpty()) {
+            "$name Chef's Choice"
+        } else {
+            "$name ${toppings.joinToString(" | ")}"
+        }
+    }
+}
+
+fun listUseCase() {
+    val noodles = Noodles()
+    val vegetables = Vegetables("Cabbage", "Sprouts", "Onion")
+    val vegetables2 = Vegetables()
+    println(noodles)
+    println(vegetables)
+    println(vegetables2)
 }

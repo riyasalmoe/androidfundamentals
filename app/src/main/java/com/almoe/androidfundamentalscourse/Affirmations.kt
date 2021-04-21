732cbb2b9fa54e7d1b56b895a44b2145f35681d1
Riyas Aboobaker <riyas.almoe@gmail.com>
2021-04-21 10:46:47 +0400

    RecyclerView progress 2

--- app/src/main/java/com/almoe/androidfundamentalscourse/Affirmations.kt
+++ app/src/main/java/com/almoe/androidfundamentalscourse/Affirmations.kt
@@ -2,6 +2,10 @@ package com.almoe.androidfundamentalscourse
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.almoe.androidfundamentalscourse.adapter.ItemAdapter
+import com.almoe.androidfundamentalscourse.data.DataSource
+
 
 class Affirmations : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,15 @@ class Affirmations : AppCompatActivity() {
         setContentView(R.layout.activity_affirmations)
 //        val textView : TextView = findViewById(R.id.Affirmation_Size)
 //        textView.text = DataSource().loadAffirmations().size.toString()
+
+        //initialize data
+        val myDataset = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
